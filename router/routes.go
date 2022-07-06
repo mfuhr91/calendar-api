62bdd0d8a8fc4d8216c69f9c0be5a3288b8ea430
Mariano Fuhr <mfuhr91@gmail.com>
2022-07-06 02:20:24 -0300

    add static resources to routes

--- router/routes.go
+++ router/routes.go
@@ -4,6 +4,7 @@ import (
 	"calendar-api/controllers"
 	"calendar-api/repositories"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var (
@@ -20,4 +21,13 @@ func InitRoutes(r *gin.Engine) {
 	api.POST("/saveEvent", cont.Save)
 	api.POST("/updateEvent", cont.Update)
 	api.POST("/deleteEvent", cont.Delete)
+
+	r.Static("static", "/static")
+	r.StaticFile("manifest.json", "/manifest.json")
+	r.StaticFile("calendar-1024.png", "/calendar-1024.png")
+	r.LoadHTMLGlob("/index.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 }

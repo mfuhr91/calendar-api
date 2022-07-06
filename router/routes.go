e48e816c25152e340906d21f9e83a341c1ca67c3
Mariano Fuhr <mfuhr91@gmail.com>
2022-07-06 02:29:30 -0300

    remove resources path from router

--- router/routes.go
+++ router/routes.go
@@ -4,7 +4,6 @@ import (
 	"calendar-api/controllers"
 	"calendar-api/repositories"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 var (
@@ -21,13 +20,4 @@ func InitRoutes(r *gin.Engine) {
 	api.POST("/saveEvent", cont.Save)
 	api.POST("/updateEvent", cont.Update)
 	api.POST("/deleteEvent", cont.Delete)
-
-	r.Static("static", "/static")
-	r.StaticFile("manifest.json", "/manifest.json")
-	r.StaticFile("calendar-1024.png", "/calendar-1024.png")
-	r.LoadHTMLGlob("/index.html")
-
-	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", nil)
-	})
 }
